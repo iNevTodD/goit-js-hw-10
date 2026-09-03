5042c53138eef25c6929c77cb8c901172d7cca77
Viktoriia <viktoriaemec4@gmail.com>
2026-09-03 19:22:46 +0200

    complete task 1

---
+++ src/js/1-timer.js
@@ -0,0 +1,82 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const startBtn = document.querySelector("[data-start]");
+startBtn.disabled = true;
+let userSelectedDate;
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0].getTime() <= new Date().getTime()) {
+            iziToast.error({
+                title: 'Error',
+                message: 'Please choose a date in the future',
+            });
+        } else {
+            startBtn.disabled = false;
+            startBtn.classList.add("active");
+            userSelectedDate = selectedDates[0];
+        }
+    },
+};
+
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+const daysPrev = document.querySelector("[data-days]");
+const hoursPrev = document.querySelector("[data-hours]");
+const minutesPrev = document.querySelector("[data-minutes]");
+const secondsPrev = document.querySelector("[data-seconds]");
+
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    startBtn.classList.remove("active");
+
+
+    setInterval(() => {
+        const currentTime = new Date().getTime();
+        const timeDifference = userSelectedDate.getTime() - currentTime;
+
+        if (timeDifference <= 0) {
+            clearInterval("#datetime-picker");
+            return;
+        }
+        const { days, hours, minutes, seconds } = convertMs(timeDifference);
+
+        daysPrev.textContent = addLeadingZero(days);
+        hoursPrev.textContent = addLeadingZero(hours);
+        minutesPrev.textContent = addLeadingZero(minutes);
+        secondsPrev.textContent = addLeadingZero(seconds);
+    }, 1000);
+
+});
+
+flatpickr("#datetime-picker", options);
